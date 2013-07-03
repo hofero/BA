f6fce4a44b2d5a5614f0bb1a2a98c28e51e39e8b
Olivia <olivia@olivia-Aspire-8730.(none)>
2013-07-03 23:11:31 +0200

    Python Programm mit Labels aus Datei

--- ba.py
+++ ba.py
@@ -22,25 +22,29 @@ if (len(args) < 2):
     print options
     sys.exit(2)
 
-#labels = noten
-labels = []
-def is_label(notes):
-    x = False
-    for note in notes:
-        if note in range(30, 91):
-            x = True
-            continue
-        else:
-            x = False
-            break
-    return x
-
-
 inputFilename = args[0]
 outputFilename = args[1]
 print options
 print "input filename", inputFilename
 print "output filename", outputFilename
+
+##labels = noten
+try:
+    lfile = open(inputFilename+"/../labels.txt","r")
+except Exception, e:
+    print"Fehler beim oeffnen der Datei"
+    sys.exit()
+
+labels = []
+for l in lfile:
+    ls = l.strip()
+    labels.append(ls);
+
+print len(labels)
+print labels
+lfile.close()
+
+
 
 #read in input files
 filenames = file(inputFilename + "/filenames.txt").readlines()
@@ -52,15 +56,18 @@ for f in filenames:
     fname = f.strip()
     if len(fname):
         try:
+            targetString = ""
             print "reading image dimensions", fname
             json_data = open(inputFilename + "/" + fname)
             data = json.load(json_data)
             transform = data["transform"]
             dims = (len(transform), len(transform[0]))
-            label = data["notes"]
-            if(is_label(label)):
-                labels.append(label)
-                targetStrings.append(label)
+            print dims
+            transcript = data["chords"]
+            for chord in transcript:
+                targetString += str(chord) +" "
+            print targetString
+            targetStrings.append(targetString)
             seqLengths.append(dims[0] * dims[1])
             seqTags.append(fname)
             seqDims.append(dims)
@@ -72,17 +79,17 @@ for f in filenames:
 #        data.remove(f)
 
 
-##inputs array mit jedem wert einzeln
-#totalLen = sum(seqLengths)
-#print "tottalLen", totalLen
-#inputs = zeros((totalLen,1), "f")
-#offset = 0
-
-#inputs array frameweise
-totalLen = sum(dim[0] for dim in seqDims)
+#inputs array mit jedem wert einzeln
+totalLen = sum(seqLengths)
 print "tottalLen", totalLen
-inputs = zeros((totalLen,seqDims[0][1]),"f")
+inputs = zeros((totalLen,1), "f")
 offset = 0
+
+##inputs array frameweise
+#totalLen = sum(dim[0] for dim in seqDims)
+#print "tottalLen", totalLen
+#inputs = zeros((totalLen,seqDims[0][1]),"f")
+#offset = 0
 
 
 for filename in seqTags:
@@ -92,17 +99,17 @@ for filename in seqTags:
     data = json.load(json_data)
     transform = data["transform"]
 
-##inputs array mit einzelnen werten fuellen
-#    for frame in transform:
-#        for i in frame:
-#            inputs[offset][0]= (float(i)-inputMean)/inputStd
-#            offset += 1
-
-
-#inputs array frameweise fuellen
+#inputs array mit einzelnen werten fuellen
     for frame in transform:
-        inputs[offset]= frame
-        offset += 1
+        for i in frame:
+            inputs[offset][0]= (float(i)-inputMean)/inputStd
+            offset += 1
+
+
+##inputs array frameweise fuellen
+#    for frame in transform:
+#        inputs[offset]= frame
+#        offset += 1
 
 
 #create a new .nc file
